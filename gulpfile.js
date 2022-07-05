01bbcfdb74ba71d12cf008b02abf92c04e9867c9
keli <15302737418@163.com>
2022-07-05 18:07:26 +0800

    fix: 修改gulpfile启动配置

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const less = require('gulp-less');
 const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const alias = require('gulp-wechat-weapp-src-alisa');
+const del = require('del');
 
 const SRC_DIR = path.join(__dirname, './src');
 const DEST_DIR = path.join(__dirname, './dist');
@@ -50,8 +51,28 @@ function transpileJsImport(cb) {
     .on('end', cb);
 }
 
-function watch(cb) {
-  gulp.watch(SRC_DIR, gulp.series(copyFiles, transpileStyles, transpileJsImport));
+async function clean(cb) {
+  await del([DEST_DIR]);
+  cb();
 }
 
-exports.default = watch;
+function build(cb) {
+  cb();
+  return gulp.series(clean, gulp.parallel(copyFiles, transpileStyles, transpileJsImport));
+}
+
+function watch(cb) {
+  const watchOptions = {
+    ignoreInitial: false
+  };
+  gulp.watch(SRC_DIR, watchOptions, build(cb));
+  cb();
+}
+
+exports.build = (cb) => {
+  build(cb);
+};
+
+exports.clean = clean;
+
+exports.watch = watch;
